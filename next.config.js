afd7bc4a85f325ba86efcf00f230da4836e0012b
nehar tale <49609699+xDeFc0nx@users.noreply.github.com>
2021-12-16 22:10:15 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://nehar:<aezakmi>@cluster0.9pfhd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  },
-};
